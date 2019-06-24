b5211808395ebd775cea4ed9704831791efc4989
Aditya Verma <adityasinghverma314@gmail.com>
2019-06-24 10:36:15 +0530

    figure out the math behind this formula

---
+++ 5_Smallest multiple.py
@@ -0,0 +1,9 @@
+from math import gcd
+a = [20, 19, 18, 17, 16, 15 , 14, 13 , 12 , 11]
+lcm = a[0]
+for i in a[1:]:
+  lcm = int(int(lcm*i)/ int (gcd(lcm, i)))
+print (lcm)
+
+
+# because lcm(a,b) * gcd(a,b) = a * b  [WHY?]
