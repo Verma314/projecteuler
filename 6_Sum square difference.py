ca81c093ed24f0ad6f6d70722364bb4393d8133d
Aditya Verma <adityasinghverma314@gmail.com>
2019-06-24 12:41:23 +0530

    updated method

--- 6_Sum square difference.py
+++ 6_Sum square difference.py
@@ -11,3 +11,10 @@ print ( sum_1 * sum_1  - sum_2)
 
 
 #are there other more efficient ways to do it? should I've used the formula?
+
+#updated:
+#method 2
+n = 100
+SUM_2 = int((n * (n+1) * (2*n + 1) ) / 6 )
+SUM_1 = (n/2) * (1 + n)
+print ( (SUM_1 * SUM_1) - SUM_2)
