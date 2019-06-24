36d32569168297798016f14194c5232cb3a12f31
Aditya Verma <adityasinghverma314@gmail.com>
2019-06-24 10:56:33 +0530

    more efficient methods?

---
+++ 6_Sum square difference.py
@@ -0,0 +1,13 @@
+
+
+sum_1 = 0
+sum_2 = 0
+
+for i in range(1,101):
+    sum_1 +=  i
+    sum_2 +=  i * i
+
+print ( sum_1 * sum_1  - sum_2)
+
+
+#are there other more efficient ways to do it? should I've used the formula?
