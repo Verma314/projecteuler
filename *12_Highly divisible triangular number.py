dbc8ff5a37d6c6756f121c7aa51462dc08b4c996
Aditya Verma <adityasinghverma314@gmail.com>
2019-06-30 10:01:58 +0530

    Update *12_Highly divisible triangular number.py

--- *12_Highly divisible triangular number.py
+++ *12_Highly divisible triangular number.py
@@ -1,12 +1,23 @@
 import math
 
+
+
+
 def factorize(n):
-    facs = []
-    for i in range(1, int(n/2) + 2):
+    count = 2
+    i = 2 
+    while ( i < n ** 0.5):
         if ( n % i == 0 ):
-            facs.append(i)
-    facs.append(n)
-    return len(facs) 
+            count += 2
+            # why adding count by 2?
+            #because you are conting only until sqrt(n). For eg: if you are trying to find all divisors for 36 -
+            #  you will count from 2 to 6. You know that 1&36,2&18, 3&12, 4&9, 6,6 are all divisors and they come in pairs.
+        i+= 1
+
+    if ( i ** 2 == n ):
+        count += 1
+    return count
+
 
 def divisors(n):
     count=2 # accounts for 'n' and '1'
@@ -22,7 +33,7 @@ def divisors(n):
 i = 1000
 while (True):
     triangle = int((i/2) * (1 + i))
-    if ( divisors(triangle) >= 500):
+    if ( factorize(triangle) >= 500):
         print(triangle)
         break
     i +=1
